59ab1a5a07874c102680357b94763ff87b74be7f
Supawadee <supawadee018@gmail.com>
2018-07-17 14:13:14 +0700

    Start App

---
+++ app/src/main/java/piw/rmutsv/ac/th/myfristapp/classroom2561/MainLab.java
@@ -0,0 +1,18 @@
+package piw.rmutsv.ac.th.myfristapp.classroom2561;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainLab extends AppCompatActivity {
+
+    //ประกาศตัวแปร
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_lab);
+       //เขียนโค้ด
+
+    }//end onCreate
+
+}// end main class
